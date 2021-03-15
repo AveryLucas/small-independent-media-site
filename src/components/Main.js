6e5b2428660600ec7e9f121f4d9b2e50b5c819db
AveryLucas <avery.lucas13@gmail.com>
2021-03-15 02:58:01 -0500

    continuing to block out layout

--- src/components/Main.js
+++ src/components/Main.js
@@ -5,54 +5,34 @@ class Main extends React.Component {
     super(props);
   }
 
-  renderArticleLink = (title = true, desc = true, date = true) => {
+  renderArticleLink = (title = true, description = true, date = true) => {
     return (
       <div className="article-link">
-        <img
-          className="article-link-image"
-          src="https://placeimg.com/1000/563/any"
-        />
-        {this.renderArticleLinkDetails(title, desc, date)}
-      </div>
-    );
-  };
-
-  renderArticleLinkDetails = (
-    title = true,
-    description = true,
-    date = true
-  ) => {
-    return (
-      <div className="article-link-details">
         {title ? <h3>Rebuilding the clone army in 7 days. A devlog.</h3> : ""}
         {description ? (
           <p className="article-sneakpeek">
-            This comprehensive guide shows how to use CSS transitions! A
-            back-to-basics look at the fundamental building blocks we need to
-            create microinteractions and other animations.
+            It may not reach the sublime heights of its predecesso.
           </p>
         ) : (
           ""
         )}
-        {date ? <p className="article-date">2 days ago</p> : ""}
       </div>
     );
   };
 
-  renderFilter = () => {
+  renderplaceholderblock(height = "100px") {
     return (
-      <div id="index-filter">
-        <div id="index-filter-category">
-          <a href="#" className="active">
-            All
-          </a>
-          <a href="#">Thoughts</a>
-          <a href="#">Reviews</a>
-          <a href="#">Devlogs</a>
-        </div>
-      </div>
+      <div
+        style={{
+          width: "100%",
+          height,
+          borderRadius: "3px",
+          background: "#C4C4C4",
+          opacity: 0.8,
+        }}
+      ></div>
     );
-  };
+  }
 
   render() {
     return (
@@ -62,22 +42,30 @@ class Main extends React.Component {
             <div id="post-slider-auto-items">
               <h2>Popular Right Now</h2>
               <div id="post-slider-auto-items-list">
-                {this.renderArticleLinkDetails(true, false, true)}
-                {this.renderArticleLinkDetails(true, false, true)}
-                {this.renderArticleLinkDetails(true, false, true)}
-                {this.renderArticleLinkDetails(true, false, true)}
-                {this.renderArticleLinkDetails(true, false, true)}
-                {this.renderArticleLinkDetails(true, false, true)}
+                {this.renderArticleLink(true, false, false)}
+                {this.renderArticleLink(true, false, false)}
+                {this.renderArticleLink(true, false, false)}
+                {this.renderArticleLink(true, false, false)}
+                {this.renderArticleLink(true, false, false)}
+                {this.renderArticleLink(true, false, false)}
               </div>
             </div>
+            {this.renderplaceholderblock("174px")}
+            {this.renderplaceholderblock()}
+            {this.renderplaceholderblock("306px")}
           </aside>
           <div id="article-list">
             <h2>Recently Published</h2>
-            {this.renderArticleLink()}
-            {this.renderArticleLink()}
-            {this.renderArticleLink()}
-            {this.renderArticleLink()}
-            {this.renderArticleLink()}
+            {this.renderArticleLink(true, true, true)}
+            {this.renderArticleLink(true, true, true)}
+            {this.renderArticleLink(true, true, true)}
+            {this.renderArticleLink(true, true, true)}
+            {this.renderArticleLink(true, true, true)}
+            {this.renderArticleLink(true, true, true)}
+            {this.renderArticleLink(true, true, true)}
+            {this.renderArticleLink(true, true, true)}
+            {this.renderArticleLink(true, true, true)}
+            {this.renderArticleLink(true, true, true)}
           </div>
         </div>
       </div>
