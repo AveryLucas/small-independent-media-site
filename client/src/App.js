cbf15e90736a11cb9ab7f7c11755eece774018d7
AveryLucas <avery.lucas13@gmail.com>
2021-03-18 11:02:52 -0500

    added react router

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,15 @@
-import logo from "./logo.svg";
 import "./App.css";
 import Main from "./components/Main";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <Main />
+      <BrowserRouter>
+        <Switch>
+          <Route path="/" component={Main} />
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
